653be2b6c443e3cabcfee6f8fba7553956532736
Rajput Somnath <somnathrajput22@gmail.com>
2024-08-14 21:26:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+const express=rquire ("express");
+const app =const  = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+    res.send("welcome to the Backend!"); 
+});
+
+app.listen(port,() =>{ 
+
+console.log('welcome to the backend !:$
+{port}');
+});
+
+
